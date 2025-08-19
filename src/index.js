5d03a74a077bdf96817f43469a2c42898f46d3ae
Mvxlzz <65513159+Mvxlzz@users.noreply.github.com>
2025-08-19 22:52:45 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TCODashboard from './TCODashboard';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TCODashboard />
+  </React.StrictMode>
+);
